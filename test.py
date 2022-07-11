69e957f2c0bad0f89b825e057a9181221aef84f7
edutjie <edutjitrahardja@gmail.com>
2022-07-12 00:31:29 +0700

    SQL Done!

---
+++ test.py
@@ -0,0 +1,26 @@
+a = ['buku', 'balpoin', 'kertas', 'penggaris', 'penghapus', 'tipp-ex']
+
+print(a[4::-2])
+print(a[-5:-3])
+print(a[:])
+print(a[-6])
+print(max(a[2:4] + ['meja']))
+
+b = {
+    'a': 1,
+    2: 'b',
+}
+
+print(b['a'])
+print(int("0xA5", 16))
+print("Hari Jum'at")
+
+a_dict = {'color': 'blue', 'fruit': 'apple', 'pet': 'dog'}
+d_items = a_dict.items()
+print(d_items)
+
+# func = lambda x: return x
+# print(func(2))
+
+print((lambda x: (x + 3) * 5 / 2)(3))
+print(100 / 25)
